cf28057746b59377364c3941a2719926e39dceed
Trent <tzeller@stevens.edu>
2019-12-02 16:55:01 -0500

    js

--- lhubjs.js
+++ lhubjs.js
@@ -1,22 +1,26 @@
-var submited = document.getElementById('submitbtn')
-var pickup = document.getElementById("pickups")
-var requests = document.getElementById("requests")
-function addRequest(e){
-    var formData = new formData(document.querySelector("form"));
-  /*  localStorage.setItem("firstName", toString(document.getElementById("firstname")))
-    localStorage.setItem("lastName", toString(document.getElementById("lastname")))
-    localStorage.setItem("phoneNumber",toString(document.getElementById("number")))
-    localStorage.setItem("building", toString(documnet.getElementById("building")))
-    localStorage.setItem("room", toString(documnet.getElementById("room")))
-    localStorage.setItem("floor", toString(documnet.getElementById("floor")))
-    localStorage.setItem("time", toString(document.getElementById("time"))) */
-
-    pickup.innerHTML = "Test"
-    requests.innerHTML = "Test"
-    addPickup()
+window.onload = function() {
+    var submited = document.getElementById('Submit')
+    var pickup = document.getElementById("pickup")
+    var requests = document.getElementById("requests")
+    
+    function addRequest(){
+        var formData = document.querySelector("form");
+      /*  localStorage.setItem("firstName", toString(document.getElementById("firstname")))
+        localStorage.setItem("lastName", toString(document.getElementById("lastname")))
+        localStorage.setItem("phoneNumber",toString(document.getElementById("number")))
+        localStorage.setItem("building", toString(documnet.getElementById("building")))
+        localStorage.setItem("room", toString(documnet.getElementById("room")))
+        localStorage.setItem("floor", toString(documnet.getElementById("floor")))
+        localStorage.setItem("time", toString(document.getElementById("time"))) */
+    
+        //pickup.innerHTML = "Test"
+        requests.innerHTML = "Test"
+        addPickup()
+    }
+    
+    function addPickup(){
+        //pickup.innerHTML = formData
+    }
+    submited.addEventListener("click", addRequest());
 }
 
-function addPickup(){
-    pickup.innerHTML = formData
-}
-submited.addEventListener("change", addRequest());
